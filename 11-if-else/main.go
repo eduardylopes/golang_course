f1c0fffaf468f77e2c5b6b6d121392b5d5ffa052
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-13 19:52:52 -0300

    feat: if else

---
+++ 11-if-else/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	number := 15
+
+	if number > 15 {
+		fmt.Println("Greater than 15")
+	} else {
+		fmt.Println("Less than 15")
+	}
+
+	// if init, the anotherNumbe variable is scoped to the statement
+	if anotherNumber := number; anotherNumber > 0 {
+		fmt.Println("Another number is greater than 0")
+	} else if anotherNumber < -10 {
+		fmt.Println("Another number is less than -10")
+	} else {
+		fmt.Println("Another number is between 0 and -10")
+	}
+}
