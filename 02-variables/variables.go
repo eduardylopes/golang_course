c6306fb109e0c5dbb43af9cf1996153ff72e6544
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-13 16:52:03 -0300

    feat: variables

---
+++ 02-variables/variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variable1 string = "Variable 1"
+	variable2 := "Variable 2"
+
+	fmt.Println(variable1)
+	fmt.Println(variable2)
+
+	var (
+		variable3 string = "Variable 3"
+		variable4 string = "Variable 4"
+	)
+
+	fmt.Println(variable3, variable4)
+
+	variable5, variable6 := "Variable 5", "Variable 6"
+	
+	fmt.Println(variable5, variable6)
+
+	const constant1 string = "Constant 1"
+	fmt.Println(constant1)
+
+	const constant2 string = "Constant 2"
+
+	variable5, variable6 = variable6, variable5
+}
