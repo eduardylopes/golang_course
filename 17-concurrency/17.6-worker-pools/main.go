0ec1445f93d31c1dc9f9a9a679b150f9f0f45169
Eduardy Lopes <eduardylopes@gmail.com>
2023-07-18 22:49:47 -0300

    feat(concurrency): implement cache in fibonacci

--- 17-concurrency/17.6-worker-pools/main.go
+++ 17-concurrency/17.6-worker-pools/main.go
@@ -1,36 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var fibTerm = 45
+var fibCache = make(map[int]int, fibTerm)
 
 func main() {
-	tasks := make(chan int, 45)
-	results := make(chan int, 45)
+	tasks := make(chan int, fibTerm)
+	results := make(chan int, fibTerm)
 
 	go worker(tasks, results)
 	go worker(tasks, results)
 	go worker(tasks, results)
-	go worker(tasks, results)
-	go worker(tasks, results)
-	go worker(tasks, results)
 
-	for i := 0; i < 45; i++ {
+	for i := 0; i < fibTerm; i++ {
 		tasks <- i
 	}
 	close(tasks)
 
-	for i := 0; i < 45; i++ {
+	start := time.Now()
+	for i := 0; i < fibTerm; i++ {
 		result := <-results
 		fmt.Println(result)
 	}
+
+	fmt.Printf("Execution time: %v\n", time.Since(start))
 }
 
 func worker(tasks <-chan int, results chan<- int) {
 	for number := range tasks {
-		results <- fibonacci(number)
+		result := fibonacci(number)
+		fibCache[number] = result
+		results <- result
 	}
 }
 
 func fibonacci(number int) int {
+	if value, ok := fibCache[number]; ok {
+		return value
+	}
+
 	if number < 2 {
 		return number
 	}
